df818e8b549b472e3a1959dad8d5ac2d0acee421
Ivan Mariic <imariic77@gmail.com>
2022-10-06 08:16:49 +0200

    Graph implemented.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
 
 function App() {
-  return <div className={styles.container}>aahahaha</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.xAxis}></div>
+      <div className={styles.yAxis}></div>
+    </div>
+  );
 }
 
 export default App;
