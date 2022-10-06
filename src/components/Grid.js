7f527f3088dcb70cee1b7da76f952f12ee5b83f7
Ivan Mariic <imariic77@gmail.com>
2022-10-06 11:57:41 +0200

    WIP.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDrop } from "react-dnd";
 import Item from "./Item";
 import styles from "./Grid.module.css";
+
+const draggableItems = [
+  { title: "Item 1", x: 30, y: 40 },
+  { title: "Item 2", x: 100, y: 200 },
+  { title: "Item 3", x: 76, y: 134 },
+];
 
 const Grid = () => {
   const [collectedProps, drop] = useDrop(() => ({
     accept: "item",
   }));
 
+  const [items, setItems] = useState(draggableItems);
+
   return (
     <div ref={drop} className={styles.container}>
       <div className={styles.xAxis}></div>
       <div className={styles.yAxis}></div>
-      <Item />
+      {items.map((item) => (
+        <Item key={item.title} item={item} />
+      ))}
     </div>
   );
 };
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,14 +2,23 @@ import React from "react";
 import { useDrag } from "react-dnd";
 import styles from "./Item.module.css";
 
-const Item = () => {
-  const [collected, drag, dragPreview] = useDrag(() => ({
+const Item = ({ item }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
     type: "item",
+    collect: (monitor) => {
+      return {
+        isDragging: monitor.isDragging,
+      };
+    },
   }));
 
   return (
-    <div ref={drag} className={styles.container}>
-      Item
+    <div
+      ref={drag}
+      className={styles.container}
+      style={{ top: item.y, left: item.x }}
+    >
+      {item.title}
     </div>
   );
 };
